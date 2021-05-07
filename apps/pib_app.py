78d0373d1515bb762008c35e7e634e3440b949bb
arnaudTurin <arnaud.turin@laplateforme.io>
2021-05-07 14:28:48 +0200

    Changement sur pib_app
    Ajout de choix multiple de pays
    Affichage des noms complets des pays

--- apps/pib_app.py
+++ apps/pib_app.py
@@ -17,10 +17,14 @@ from app import app
 
 df_pib = pd.read_csv("data/PIB.csv")
 df_pib = df_pib[['LOCATION', 'TIME', 'Value']]
-liste_pays = df_pib['LOCATION'].unique()
-temps = df_pib['TIME'].unique()
 
-
+# temps = df_pib['TIME'].unique()
+df = pd.read_csv("data/emploi.csv")
+nom_pays = df[['LOCATION', "Pays"]].drop_duplicates()
+liste_pays = nom_pays['Pays'].values.tolist()
+liste_pays.sort()
+df_pib = pd.merge(df_pib, nom_pays, on = "LOCATION", how = "inner")
+    
 # colors = {
 #     'background': 'rgba(255, 255, 128, .5)',
 #     'text': '#7FDBFF'
@@ -42,8 +46,8 @@ layout = html.Div(children=[
     dcc.Dropdown(
                 id='selection_pays',
                 options=[{'label': pays, 'value': pays} for pays in liste_pays],
-                value='OECD', style={'width':'30%'}, className='row',
-                # multi=True
+                value=['OCDE - Total'], style={'width':'30%'}, className='row',
+                multi=True
             ),
 ])
 
@@ -52,17 +56,18 @@ layout = html.Div(children=[
     [Input('selection_pays', 'value')])
 def update_figure(choix_pays):
     
-    
-    df_filter = df_pib[df_pib.LOCATION == choix_pays]
-    # Création de la trame 1
-    fig = make_subplots()
-    fig.add_trace(go.Scatter(
+    fig = go.Figure()
+    for pays in choix_pays:
+        df_filter = df_pib[df_pib.Pays == pays]
+        fig.add_trace(go.Scatter(
                     x = df_filter.TIME,
                     y = df_filter.Value,
                     mode = "lines",
-                    name = "citations",
-                    marker = dict(color = 'red'),   #rgba(16, 112, 2, 1)
+                    name = pays,
+                    #marker = dict(color = 'red'),   #rgba(16, 112, 2, 1)
                     line = dict(width=4)))
+    
+    
 
 
     fig.update_layout(title = 'PIB trimestriel',
